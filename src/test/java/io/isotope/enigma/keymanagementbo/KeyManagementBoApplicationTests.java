c865b48df873b57088610a2e9ae85dce66ccdd7c
djovel <djvelickovic234@gmail.com>
2020-04-12 12:06:14 +0200

    Project shell.

---
+++ src/test/java/io/isotope/enigma/keymanagementbo/KeyManagementBoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.isotope.enigma.keymanagementbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyManagementBoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
